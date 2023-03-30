faa3bc2347273e3c2225a1527950ab9781ef52c9
Sascha Oberhofer <sascha.oberhofer@mobi.ch>
2023-03-30 16:46:40 +0200

    Push first time to Repo

---
+++ WeeeknumberinTaskbar2/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System.Globalization;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace WeeeknumberinTaskbar2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            showToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            minimizeToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Visible = true;
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            notifyIcon1.Text = "Week " + GetIso8601WeekOfYear(DateTime.Now);
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { showToolStripMenuItem, minimizeToolStripMenuItem, exitToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(124, 70);
+            // 
+            // showToolStripMenuItem
+            // 
+            showToolStripMenuItem.Name = "showToolStripMenuItem";
+            showToolStripMenuItem.Size = new Size(123, 22);
+            showToolStripMenuItem.Text = "show";
+            showToolStripMenuItem.Click += OnShow;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(123, 22);
+            exitToolStripMenuItem.Text = "exit";
+            exitToolStripMenuItem.Click += OnExit;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 79);
+            label1.TabIndex = 1;
+            label1.Text = "Week " + GetIso8601WeekOfYear(DateTime.Now);
+            label1.Click += label1_Click;
+            // 
+            // minimizeToolStripMenuItem
+            // 
+            minimizeToolStripMenuItem.Name = "minimizeToolStripMenuItem";
+            minimizeToolStripMenuItem.Size = new Size(123, 22);
+            minimizeToolStripMenuItem.Text = "minimize";
+            minimizeToolStripMenuItem.Click += OnMinimize;
+            // 
+            // Form1
+            // 
+
+         
+            Width = 300;
+            Height = 200;
+            Opacity = 0;
+            ShowInTaskbar = false;
+            Hide();
+            ClientSize = new Size(495, 198);
+            ContextMenuStrip = contextMenuStrip1;
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.WindowsDefaultBounds;
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            notifyIcon1.Dispose();
+            Application.Exit();
+        }
+
+         private void OnShow(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Opacity = 100;
+            this.Height = 200;
+            this.Width = 400;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+
+            this.Show();
+        }
+        private void OnMinimize(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private int GetIso8601WeekOfYear(DateTime time)
+        {
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                time = time.AddDays(3);
+            }
+
+            // Calculate the week number according to ISO 8601
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+        #endregion
+
+        private NotifyIcon notifyIcon1;
+
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem showToolStripMenuItem;
+        private Label label1;
+        private ToolStripMenuItem minimizeToolStripMenuItem;
+    }
+}
---
+++ WeeeknumberinTaskbar2/Form1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Windows.Forms;
+
+namespace WeeeknumberinTaskbar2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
